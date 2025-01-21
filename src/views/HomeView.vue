98d63179ce4080eaede5ed214f133c1ef4f7769e
nathan <nathan.garcia@etu.univ-lyon1.fr>
2025-01-21 11:56:09 +0100

    init(tailwind): add tailwind in project & clear default page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="text-6xl font-bold underline text-red-600">
+      Hello world!
+    </h1>
   </main>
 </template>
+
+<script setup lang="ts">
+
+</script>
